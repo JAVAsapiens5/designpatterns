e2ac05066013d0681f14ad52010c6cbb90c2717a
Marcin Szupke <marcin.szupke.sda@gmail.com>
2021-07-03 14:15:02 +0200

    builder - task

---
+++ src/main/java/builder/task/Task.java
@@ -0,0 +1,13 @@
+package builder.task;
+
+import java.util.Date;
+
+// Bug, Feature, Story, ...
+public class Task {
+
+    private int id;
+    private String summary;
+    private String description;
+    private boolean done;
+    private Date dueDate;
+}
